941f34ae620b0bfbccf1dfb71c5623976c801c45
Rasmus Listra <rasmus.listra@netgroup.ee>
2015-08-05 16:46:22 +0300

    Performance test

--- src/test/java/runner/test/HeaderSearchTest.java
+++ src/test/java/runner/test/HeaderSearchTest.java
@@ -1,15 +1,22 @@
 package runner.test;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HeaderSearchTest {
+    public static FirefoxDriver driver;
+
+    @BeforeClass
+    public void openBrowser() {
+        driver = new FirefoxDriver();
+    }
 
     @Test
     public void headerSearch() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://oxygen.netgroupdigital.com/#");
+
         // driver.findElement(By.cssSelector(".form-control.ng-pristine.ng-untouched.ng-valid")).sendKeys(GlobalConstants.SEARCH_IN_HEADER_FOR_Matemaatika);
         // driver.findElement(By.cssSelector(".btn.btn-default")).click();
 
